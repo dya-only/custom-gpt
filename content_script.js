62bdb51d1c63a21538d6d677ad956c17f6c60990
Boseok <dyacode@pmh.codes>
2023-12-20 01:06:48 +0900

    feat: upload img

--- content_script.js
+++ content_script.js
@@ -1,42 +1,43 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "changeColor") {
     if (request.value) {
       const nav = document.querySelector('#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.flex-1.overflow-hidden > div > div.absolute.left-0.right-0 > div')
-      const nav2 = document.querySelector('#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.flex-1.overflow-hidden > div > div.absolute.left-0.right-0 > div')
       const main = document.querySelector('main')
-      if (main) {
-        const bg = new Image()
-        bg.src = 'https://chatgpt-custom.s3.ap-northeast-2.amazonaws.com/ash_4k.png'
-        bg.style.width = '100%'
-        bg.style.height = '100%'
-        bg.style.position = 'absolute'
-        bg.style.filter = 'brightness(50%)'
-        bg.style.objectFit = 'cover'
-        main.prepend(bg)
 
-        nav.style.backgroundColor = 'transparent'
-        nav.style.backdropFilter = 'blur(10px)'
-        nav2.style.backgroundColor = 'transparent'
-        nav2.style.backdropFilter = 'blur(10px)'
+      if (main) {
+        chrome.storage.local.get(['switch', 'img'], (result) => {
+          const bg = new Image()
+          bg.className = 'bg'
+          bg.src = result.img
+          bg.style.width = '100%'
+          bg.style.height = '100%'
+          bg.style.position = 'absolute'
+          bg.style.filter = 'brightness(50%)'
+          bg.style.objectFit = 'cover'
+          main.prepend(bg)
+
+          nav.style.backgroundColor = 'transparent'
+          nav.style.backdropFilter = 'blur(10px)'
+        })
+
+        window.location.reload()
       }
     } else {
-      const main = document.querySelector('main')
-      if (main) {
-        document.querySelector('main > img').style.display = none
-      }
+      document.querySelector('main > img').remove()
+      window.location.reload()
     }
   }
 })
 
-chrome.storage.local.get(['switch'], function (result) {
+chrome.storage.local.get(['switch', 'img'], (result) => {
   if (result.switch !== undefined) {
     if (result.switch) {
       const nav = document.querySelector('#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.flex-1.overflow-hidden > div > div.absolute.left-0.right-0 > div')
-      const nav2 = document.querySelector('#__next > div.relative.z-0.flex.h-full.w-full.overflow-hidden > div.relative.flex.h-full.max-w-full.flex-1.flex-col.overflow-hidden > main > div.flex.h-full.flex-col > div.flex-1.overflow-hidden > div > div.absolute.left-0.right-0 > div')
       const main = document.querySelector('main')
       if (main) {
         const bg = new Image()
-        bg.src = 'https://chatgpt-custom.s3.ap-northeast-2.amazonaws.com/ash_4k.png'
+        bg.className = 'bg'
+        bg.src = result.img  // 'https://chatgpt-custom.s3.ap-northeast-2.amazonaws.com/ash_4k.png'
         bg.style.width = '100%'
         bg.style.height = '100%'
         bg.style.position = 'absolute'
@@ -46,14 +47,9 @@ chrome.storage.local.get(['switch'], function (result) {
 
         nav.style.backgroundColor = 'transparent'
         nav.style.backdropFilter = 'blur(10px)'
-        nav2.style.backgroundColor = 'transparent'
-        nav2.style.backdropFilter = 'blur(10px)'
       }
     } else {
-      const main = document.querySelector('main')
-      if (main) {
-        document.querySelector('main > img').style.display = none
-      }
+      document.querySelector('main > img').remove()
     }
   }
 })
--- popup.js
+++ popup.js
@@ -1,19 +1,38 @@
 document.querySelector('.switch').addEventListener('click', () => {
-  chrome.storage.local.set({ switch: document.querySelector('.switch > input').checked }, function () {
-    console.log('디자인 변경 상태가 저장되었습니다:', document.querySelector('.switch > input').checked)
+  chrome.storage.local.set({ switch: document.querySelector('.switch > input').checked }, () => {
+    console.log('Design Changed: ', document.querySelector('.switch > input').checked)
   })
 
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, { action: "changeColor", value: document.querySelector('.switch > input').checked })
   })
 })
 
-chrome.storage.local.get(['switch'], function(result) {
+document.querySelector('.uploader').addEventListener('change', () => {
+  const file = document.querySelector('.uploader').files[0]
+
+  if (file) {
+    let reader = new FileReader()
+    reader.onloadend = () => {
+      chrome.storage.local.set({ img: reader.result }, () => {
+        console.log('Image Changed: ', reader.result)
+
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.tabs.sendMessage(tabs[0].id, { action: "changeColor", value: document.querySelector('.switch > input').checked })
+        })
+      })
+    }
+
+    reader.readAsDataURL(file)
+  }
+})
+
+chrome.storage.local.get(['switch', 'img'], (result) => {
   if (result.switch !== undefined) {
     document.querySelector('.switch > input').checked = result.switch;
 
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "changeColor", value: document.querySelector('.switch > input').checked});
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "changeColor", value: document.querySelector('.switch > input').checked });
     })
   }
 })
